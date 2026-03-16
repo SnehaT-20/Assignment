ddfc810288b9ce53b04946407f52758766d92ad1
Sneha Timra <snehatimra@gmail.com>
2026-03-16 12:05:59 +0530

    q14.py

--- python/Assignment/q14.py
+++ python/Assignment/q14.py
@@ -1,5 +1,7 @@
 '''Write a python program to sum of the first n positive integers.'''
 
 n = int(input("Enter Your number := "))
-answer = n*(n+1)//2
-print(f"Your first n positive integer sum is := {answer}")
+sum = 0
+for i in range(1, n+1):
+    sum += i
+    print(f"Your first n positive integer sum is := {sum}")
